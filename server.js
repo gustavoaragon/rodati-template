5baf2a9ea70ec4edf2c17c62cfc5cb2233709cb1
Leandro D'Onofrio <leandrodonofrio@gmail.com>
2014-05-08 20:39:16 -0300

    Throw an error if the configuration file is not found

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 'use strict';
 
 //Enviroment
-var env  = process.env.NODE_ENV || 'development';
+var env   = process.env.NODE_ENV || 'development';
 
 //Configuration
 var path  = require('path');
+var fs    = require('fs');
 var nconf = require('nconf');
 var root  = path.normalize(__dirname);
-nconf.argv().env().file({ file: root + '/server/config/' + env + '.json' });
-nconf.set('app:paths', {
-    'root': root,
-    'server': root + '/server',
-    'public': root + '/public'
-});
-nconf.set('app:port', (Number(process.env.PORT) || nconf.get('app:port')));
+var file  = root + '/server/config/' + env + '.json';
 
-//Server
-module.exports = require(root + '/server/libraries/express')();
+//Check if exist the configuration file
+fs.exists(file, function(exists) {
+
+    if(exists) {
+
+        //Set the configuration
+        nconf.argv().env().file({ file: file });
+        nconf.set('app:paths', {
+            'root': root,
+            'server': root + '/server',
+            'public': root + '/public'
+        });
+        nconf.set('app:port', (Number(process.env.PORT) || nconf.get('app:port')));
+
+        //Server
+        module.exports = require(root + '/server/libraries/express')();
+
+    } else {
+
+        var err = new Error('Configuration file not found. Check the config folder.');
+
+        throw err;
+
+    }
+
+});
