620d6b987105b00e534a65a6d35494e60bb9c0f3
Leandro D'Onofrio <leandrodonofrio@gmail.com>
2014-09-11 14:52:43 -0300

    Send to new relic winston.error

--- server/core/winston.js
+++ server/core/winston.js
@@ -1,7 +1,10 @@
+/*eslint no-unused-vars:0*/
+
 'use strict';
 
 //Main dependencies
 var winston = require('winston');
+var winstonNewrelic = require('winston-newrelic');
 
 exports.init = function(app, config) {
 
@@ -13,5 +16,14 @@ exports.init = function(app, config) {
 
 	//Set logging into the filesystem
 	winston.add(winston.transports.File, config.app.logger.file);
+
+	//Enable newrelic loggin based on the configuration
+	if (config.app.newrelic.enabled === true) {
+
+		winston.add(winston.transports.newrelic, {
+			newrelic: require('newrelic')
+		});
+	
+	}
 
 };
