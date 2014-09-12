b192fae95bbea2bccc451480359ecee6b1783376
Leandro D'Onofrio <leandrodonofrio@gmail.com>
2014-09-12 16:46:25 -0300

    Separation of tests

---
+++ test/functional/404/index.js
@@ -0,0 +1,17 @@
+/*global casper */
+
+casper.test.begin('404 page', 2, function suite(test) {
+
+    var url = 'http://localhost:3000/';
+    
+    //404
+    casper.thenOpen(url + '404', function(response) {
+        casper.test.assertHttpStatus(404, 'Status 404');
+        casper.test.assertMatch(response.contentType, /html/, 'Content type HTML');
+    });
+
+    casper.run(function() {
+        test.done();
+    });
+
+});
---
+++ test/functional/hello/index.js
@@ -0,0 +1,17 @@
+/*global casper */
+
+casper.test.begin('Hello page', 2, function suite(test) {
+
+    var url = 'http://localhost:3000/';
+
+    //Index
+    casper.start(url, function(response) {
+        casper.test.assertHttpStatus(200, 'Status 200');
+        casper.test.assertMatch(response.contentType, /html/, 'Content type HTML');
+    });
+
+    casper.run(function() {
+        test.done();
+    });
+
+});
