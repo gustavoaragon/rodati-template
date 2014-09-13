bccabb4cf73ed6b7cf744dae51582d3bcba61a58
Leandro D'Onofrio <leandrodonofrio@gmail.com>
2014-09-13 19:29:38 -0300

    Fix newrelix helper

--- server/helpers/dust/_newrelic.js
+++ server/helpers/dust/_newrelic.js
@@ -1,19 +1,20 @@
 'use strict';
 
 /**
+ * Global modules
+ */
+var newrelic = require('newrelic');
+
+/**
  * Set the New Relic Browser Monitoring if is enabled
  * @param  {object} dust   Dust intance of the app
  * @return {string}        Return all the code of Browser Timing Header of New Relic
  */
 module.exports = function(dust) {
 
-	dust.helpers.newrelic = function(chunk, context) {
+	dust.helpers.newrelic = function(chunk) {
 
-		var browserTimingHeader = '';
-
-		if (context.stack.head.newrelic) {
-			browserTimingHeader = context.stack.head.newrelic.getBrowserTimingHeader();
-		}
+		var browserTimingHeader = newrelic.getBrowserTimingHeader();
 
 		return chunk.write(browserTimingHeader);
 
