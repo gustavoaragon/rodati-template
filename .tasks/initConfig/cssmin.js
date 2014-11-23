59a82c986e334f20b2c861e8a2cdcfd80362e55a
Leandro D'Onofrio <leandrodonofrio@gmail.com>
2014-11-23 12:40:12 -0300

    Fix optimize with files with multiple dots

--- .tasks/initConfig/cssmin.js
+++ .tasks/initConfig/cssmin.js
@@ -5,7 +5,8 @@ module.exports = function(grunt) {
 	return {
 		default: {
 			files: grunt.file.expandMapping(['public/css/pages/*.css', 'public/css/vendor/*.css', '!public/css/pages/*.min.css', '!public/css/vendor/*.min.css'], '.', {
-				ext: '.min.css'
+				ext: '.min.css',
+				extDot: 'last'
 			})
 		}
 	};
--- .tasks/initConfig/uglify.js
+++ .tasks/initConfig/uglify.js
@@ -5,9 +5,8 @@ module.exports = function(grunt) {
 	return {
 		default: {
 			files: grunt.file.expandMapping(['public/js/pages/*.js', '!public/js/pages/*.min.js', 'public/js/vendor/*.js', '!public/js/vendor/*.min.js'], '.', {
-				rename: function(destBase, destPath) {
-					return destPath.replace('.js', '.min.js');
-				}
+				ext: '.min.js',
+				extDot: 'last'
 			})
 		}
 	};
